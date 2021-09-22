39606bc6556108fea2d58e29d5a726b675cd7f23
NaTaeKoon <smathj@naver.com>
2021-09-23 00:12:46 +0900

    Python module Study

---
+++ module/mod1.py
@@ -0,0 +1,16 @@
+#python 3.9 Version
+#python.exe -m pip install --upgrade pip
+
+# mod1.py
+def add(a,b) :
+    return a+b
+
+def sub(a,b) :
+    return a - b
+
+# python mod1.py 로 했을때만 동작함
+if __name__ == "__main__":
+    print(add(1,4))
+    print(sub(4,2))
+
+
